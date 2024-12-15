f803f31dfbe133272efda2f4b1a873da6a45312d
linsmek <lina.mekouar1@gmail.com>
2024-12-15 14:38:15 -0800

    Add files via upload

--- RAG_simple.py
+++ RAG_simple.py
@@ -13,6 +13,8 @@ from langchain.schema import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from streamlit.runtime.uploaded_file_manager import UploadedFile
 
+import chromadb
+from chromadb.utils.embedding_functions.ollama_embedding_function import OllamaEmbeddingFunction
 # Set page config only once at the start of the script
 st.set_page_config(page_title="RAG Question Answer")
 
@@ -32,14 +34,52 @@ def process_document(uploaded_file: UploadedFile) -> list[Document]:
     )
     return text_splitter.split_documents(docs)
 
+def get_vector_collection() -> chromadb.Collection:
+
+    ollama_ef = OllamaEmbeddingFunction(
+        url="http://localhost:11434/api/embeddings",
+        model_name="nomic-embed-text:latest",
+    )
+
+    chroma_client = chromadb.PersistentClient(path="./demo-rag-chroma")
+    return chroma_client.get_or_create_collection(
+        name="rag_app",
+        embedding_function=ollama_ef,
+        metadata={"hnsw:space": "cosine"},
+    )
+
+def add_to_vector_collection(all_splits: list[Document], file_name: str):
+
+    collection = get_vector_collection()
+    documents, metadatas, ids = [], [], []
+
+    for idx, split in enumerate(all_splits):
+        documents.append(split.page_content)
+        metadatas.append(split.metadata)
+        ids.append(f"{file_name}_{idx}")
+
+    collection.upsert(
+        documents=documents,
+        metadatas=metadatas,
+        ids=ids,
+    )
+    st.success("Data added to the vector store!")
+
+
 if __name__ == "__main__":
     with st.sidebar:
+        st.set_page_config(page_title="RAG Question Answer")
+        st.header("🗣️ RAG Question Answer")
         uploaded_file = st.file_uploader(
             "**📑 Upload PDF files for QnA**", type=["pdf"], accept_multiple_files=False
         )
         process = st.button("⚡️ Process")
 
     if uploaded_file and process:
-        all_splits = process_document(uploaded_file)
-        st.write(all_splits)
+        if uploaded_file and process:
+            normalize_uploaded_file_name = uploaded_file.name.translate(
+                str.maketrans({"-": "_", ".": "_", " ": "_"})
+            )
+            all_splits = process_document(uploaded_file)
+            add_to_vector_collection(all_splits, normalize_uploaded_file_name)
 
